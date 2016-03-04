1e34a7ab1d0d5ca92bcd0d12e24efb652338eb1e
rafaeleliasrb <13rafael.elias@gmail.com>
2016-03-03 23:10:07 -0300

    First commit

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,13 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http){
+	$scope.fotos = [];
+	$scope.filtro = '';
+
+	$http.get('/v1/fotos')
+	.success(function(retorno) {
+		console.log(retorno);
+		$scope.fotos = retorno;
+	})
+	.error(function error(erro) {
+		console.log(erro);
+	});
+});
