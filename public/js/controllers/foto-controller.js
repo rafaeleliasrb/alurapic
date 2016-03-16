a74aad993cf7b3e27d2da587593d5e7b8d2a3d9d
rafaeleliasrb <13rafael.elias@gmail.com>
2016-03-15 22:22:28 -0300

    Aula7

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,18 +1,41 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http) {
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams) {
 	$scope.foto = {};
 	$scope.mensagem = '';
 
+	if($routeParams.fotoId) {
+		$http.get('/v1/fotos/' + $routeParams.fotoId)
+		.success(function(foto) {
+			$scope.foto = foto;
+		})
+		.error(function(erro) {
+			console.log(erro);
+			$scope.mensagem = 'Não foi possível receber a foto.'
+		});
+	}
+
 	$scope.submeter = function() {
 		if($scope.formulario.$valid) {
-			$http.post('v1/fotos', $scope.foto)
-			.success(function() {
-				$scope.foto = {};
-				$scope.mensagem = 'Foto incluida com sucesso';
-			})
-			.error(function(erro) {
-				$scope.mensagem = 'Problema ao incluir a foto';
-				console.log(erro);
-			});
+			if($routeParams.fotoId) {
+				$http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+				.success(function() {
+					$scope.mensagem = 'Foto alterada com sucesso';
+				})
+				.error(function(erro) {
+					$scope.mensagem = 'Problema ao alterar a foto';
+					console.log(erro);
+				});
+			}
+			else {
+				$http.post('v1/fotos', $scope.foto)
+				.success(function() {
+					$scope.foto = {};
+					$scope.mensagem = 'Foto incluida com sucesso';
+				})
+				.error(function(erro) {
+					$scope.mensagem = 'Problema ao incluir a foto';
+					console.log(erro);
+				});
+			}
 		}
 	};
 });
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -10,4 +10,17 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 	.error(function error(erro) {
 		console.log(erro);
 	});
+
+	$scope.remover = function(foto) {
+		$http.delete('/v1/fotos/' + foto._id)
+		.success(function() {
+			var indiceFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indiceFoto, 1);
+			$scope.mensagem = 'Foto: ' + foto.titulo + ' removida com sucesso.';
+		})
+		.error(function(erro) {
+			console.log(erro);
+			$scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo;
+		});
+	};
 });
